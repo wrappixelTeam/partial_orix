094899ad5737fa4748a1c53a0eda86f3b3ec4de3
adminmart <info@adminmart.com>
2023-12-18 10:06:50 +0530

    commit message

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -3,25 +3,7 @@ $(function () {
 
   $(".preloader").fadeOut();
 
-  // =================================
-  // Tooltip
-  // =================================
-  var tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
 
-  // =================================
-  // Popover
-  // =================================
-  var popoverTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="popover"]')
-  );
-  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl);
-  });
 
   // increment & decrement
   $(".minus,.add").on("click", function () {
